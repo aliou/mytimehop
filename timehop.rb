0cf742fb78924edbdefcca55a26c295bd6e73ab7
Aliou Diallo <diallo_e@epitech.net>
2013-02-18 09:30:26 +0100

    Initial instagram support.

--- timehop.rb
+++ timehop.rb
@@ -2,15 +2,19 @@ require 'date'
 require_relative 'database'
 require_relative 'mail'
 
+@today = (DateTime.now.to_date - 366).strftime('%s').to_i
+@omorrow = @today + 3600 * 24
+
 def get_tweets
-  res = []
-  today = (DateTime.now.to_date - 366).strftime('%s').to_i
-  tomorrow = today + 3600 * 24
-  tweets = DB.fetch("Select * from tweets
-  where date >= #{today} AND date < #{tomorrow}")
+  tweets = DB.fetch("SELECT * FROM tweets
+  WHERE date >= #{@today} AND date < #{@tomorrow}")
 
   tweets.to_a
 end
 
 def get_instagram
+  query = DB.fetch("SELECT * FROM instagram
+  where date >= #{@today} AND date < #{@tomorrow}")
+
+  picts = query.to_a
 end
