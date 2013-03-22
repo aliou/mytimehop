a4c500843966aa2ff543760ed52c32064158a0e8
Aliou Diallo <aliou.diallo@me.com>
2013-03-22 09:11:33 +0100

    Remove unused variable and fix date of tweets.

--- timehop.rb
+++ timehop.rb
@@ -4,8 +4,7 @@ require_relative 'mail'
 require_relative 'database'
 
 def get_tweets
-  res = []
-  today = (DateTime.now.to_date - 366).strftime('%s').to_i
+  today = (DateTime.now.to_date - 365).strftime('%s').to_i
   tomorrow = today + 3600 * 24
   query = DB.fetch("Select * from tweets
   where date >= #{today} AND date < #{tomorrow}")
