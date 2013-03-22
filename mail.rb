0826b5401311a9976e06cb1709c53172afea3a37
Aliou Diallo <aliou.diallo@me.com>
2013-03-22 09:11:33 +0100

    Fix date.

--- mail.rb
+++ mail.rb
@@ -6,10 +6,11 @@ def send_mail(tweets)
   tweets.each do |tw|
     str += "#{tw[:text]}<br /><small>#{tw[:created_at]}</small><br /><hr />"
   end
+
   Gmail.connect(ENV["bot_mail"], ENV["bot_passwd"]) do |gmail|
     gmail.deliver do
       to ENV["my_mail"]
-      subject "Your tweets on #{(DateTime.now.to_date - 366).to_s}."
+      subject "Your tweets on #{(DateTime.now.to_date - 365).to_s}."
       html_part do
 	content_type 'text/html; charset=UTF-8'
 	body str
